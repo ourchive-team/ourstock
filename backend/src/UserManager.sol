b1fd8f0139cf527ea782c276235f07db81575881
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-15 10:49:48 +0900

    Implement nickname duplicate check
    * Remove the previous mapping
    * Add new data structures to store user nicknames
    * Add `nicknameAlreadyExists` function

--- backend/src/UserManager.sol
+++ backend/src/UserManager.sol
@@ -4,19 +4,30 @@ error Unauthorized();
 error EmptyNicknameNotAllowed();
 
 contract UserManager {
-    mapping(address => string) userNicknames;
+    string[] public nicknames;
+    mapping(address => uint256) userNicknameIds;
 
-    function getUserNickname(address _id) external view returns (string memory) {
-        return userNicknames[_id];
+    function getUserNickname(address _account) external view returns (string memory) {
+        return nicknames[userNicknameIds[_account]];
     }
 
-    function setUserNickname(address _id, string memory _nickname) public {
-        if (msg.sender != _id) {
+    function nicknameAlreadyExists(string memory _nickname) external view returns (bool) {
+        for (uint256 i = 0; i < nicknames.length; i++) {
+            if (keccak256(abi.encodePacked(nicknames[i])) == keccak256(abi.encodePacked(_nickname))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function setUserNickname(address _account, string memory _nickname) public {
+        if (msg.sender != _account) {
             revert Unauthorized();
         }
         if (bytes(_nickname).length == 0) {
             revert EmptyNicknameNotAllowed();
         }
-        userNicknames[_id] = _nickname;
+        userNicknameIds[_account] = nicknames.length;
+        nicknames.push(_nickname);
     }
 }
