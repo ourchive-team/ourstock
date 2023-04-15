95e56d907f7e6d65271e8061d3dcf2a217c16329
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-15 14:31:16 +0900

    Implement `uploadImage`

--- backend/test/Marketplace.t.sol
+++ backend/test/Marketplace.t.sol
@@ -9,4 +9,15 @@ contract MarketplaceTest is Test {
     function setUp() public {
         marketplace = new Marketplace();
     }
+
+    function testUploadImage(
+        uint256 _price,
+        string calldata _name,
+        string calldata _description,
+        string calldata _uri,
+        uint256 _expiry
+    ) public {
+        vm.prank(msg.sender);
+        marketplace.uploadImage(_price, _name, _description, _uri, _expiry);
+    }
 }
