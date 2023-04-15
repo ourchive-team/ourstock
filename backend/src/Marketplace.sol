c018ba125810119cdb74244e8477fd2ce4bc2de8
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-15 12:44:58 +0900

    Define `StockImage` structure

--- backend/src/Marketplace.sol
+++ backend/src/Marketplace.sol
@@ -2,6 +2,18 @@ pragma solidity ^0.8.17;
 
 import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
+struct StockImage {
+    uint256 id;
+    string name;
+    string description;
+    string uri;
+    uint256 price;
+    address creator;
+    uint256 expiry;
+}
+
 contract Marketplace is ERC1155 {
+    StockImage[] public stock_images;
+
     constructor() ERC1155("") {}
 }
