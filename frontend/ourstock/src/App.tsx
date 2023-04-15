cf7cd530aa0cac33aa14507374728cb162280ff4
Sanghyeok (Dave Kim) <Sanghyeok.kim@alphanonce.com>
2023-04-15 23:55:25 +0900

    add profilePage and uploadedListPage

--- frontend/ourstock/src/App.tsx
+++ frontend/ourstock/src/App.tsx
@@ -6,6 +6,10 @@ import LoginPage from "./Pages/AuthPage/LoginPage";
 import NicknameRegistrationPage from "./Pages/AuthPage/NicknameRegistrationPage";
 import MainPage from "./Pages/MainPage";
 import LoginWrapper from "./Components/LoginWrapper";
+import UploadPage from "./Pages/UploadPage";
+import ImageDetailsPage from "./Pages/ImageDetailsPage";
+import PurchasePage from "./Pages/ImageDetailsPage/PurchasePage";
+import ProfilePage from "./Pages/ProfilePage";
 
 const App = () => {
   return (
@@ -15,6 +19,10 @@ const App = () => {
                   <Route path="/" element={<LoginPage/>} />
                   <Route path="/nickname" element={<NicknameRegistrationPage />} />
                   <Route path="/main" element={<MainPage />} />
+                  <Route path="/upload" element={<UploadPage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/images/:creator/:nickname/:title" element={<ImageDetailsPage />} />
+                  <Route path="/images/:creator/:nickname/:title/purchase" element={<PurchasePage />} />
               </Routes>
           </LoginWrapper>
       </BrowserRouter>
---
+++ frontend/ourstock/src/Pages/UploadListPage/index.tsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { useRecoilState } from 'recoil';
+import {addressState} from "../../states/loginState";
+import {onchain} from "../../func";
+import TopNavigator from "../../Components/NavigatorComponents/TopNavigator";
+import {baseColor, PaddingBox, StyledSpan} from '../../styles';
+import ImageContainer from "../../Components/ImageComponents/ImageContainer";
+import BottomContainer from "../../Components/NavigatorComponents/BottomContainer";
+import {TokenItem} from "../../Components/ImageComponents/ImageSkeletonRenderer";
+
+interface ItemList {
+  id: string;
+  title: string;
+  desc: string;
+  profit: string;
+}
+
+const UploadListPage = () => {
+  const [address, setAddress] = useRecoilState(addressState);
+
+
+
+    const [uploadedImage, setUploadedList] = useState<TokenItem[]>([
+        {
+            creator: '0x00000000',
+            creatorNickname: 'test',
+            name: 'test',
+            collection: 'test',
+            price: 0,
+            uri: 'test',
+        },
+        {
+            creator: '0x00000000',
+            creatorNickname: 'test',
+            name: 'test',
+            collection: 'test',
+            price: 0,
+            uri: 'test',
+        },
+    ]);
+
+  const addressString = address;
+
+  useEffect(() => {
+    // onchain.getUploadedImageList(addressString).then(data => {
+    //   setUploadedImage(data);
+    // });
+  }, []);
+
+  const nav = useNavigate();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <TopNavigator>
+        <span>Upload List</span>
+      </TopNavigator>
+      <PaddingBox>
+        {uploadedImage.map(el => {
+          return (
+            <div
+              onClick={() => nav(`/images/${el.creator}/${el.creatorNickname}/${el.name}`)}
+              style={{
+                display: 'flex',
+                marginBottom: '20px',
+                border: '1px solid black',
+                borderRadius: '16px',
+                height: 'fit-content',
+              }}
+            >
+              <ImageContainer uri={el.uri} alt={el.name} style={{ width: 'fit-content', maxWidth: '136px' }} />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: '16px 16px 16px 0px',
+                  height: '140px',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                }}
+              >
+                <StyledSpan style={{ fontWeight: 700, fontSize: '15px', whiteSpace: 'nowrap' }}>{el.name}</StyledSpan>
+                <StyledSpan
+                  style={{
+                    fontWeight: 400,
+                    fontSize: '12px',
+                    width: '100%',
+                    margin: '8px 0px',
+                    color: 'rgba(0,0,0,0.6)',
+                  }}
+                >
+                  description todo
+                </StyledSpan>
+
+                <div style={{ display: 'flex', alignItems: 'center', marginTop: 'auto' }}>
+                  <span style={{ fontSize: '10px', color: 'rgba(0,0,0,0.4)', marginRight: '8px' }}>My profit</span>
+                  <span style={{ fontWeight: 700, color: '#D57368' }}>1234</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </PaddingBox>
+      <BottomContainer
+        style={{
+          position: 'fixed',
+          left: 0,
+          bottom: 0,
+          backgroundColor: baseColor.beige,
+          zIndex: 2,
+          paddingTop: '0px',
+        }}
+      />
+    </div>
+  );
+};
+
+export default UploadListPage;
