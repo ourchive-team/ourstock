d181982a561eec4cb1f3870902744949ad69323e
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-15 14:31:16 +0900

    Implement `uploadImage`

--- backend/src/Marketplace.sol
+++ backend/src/Marketplace.sol
@@ -13,7 +13,30 @@ struct StockImage {
 }
 
 contract Marketplace is ERC1155 {
+    uint256 public latest_id = 0;
     StockImage[] public stock_images;
+    mapping(address => uint256[]) creator_to_stock_image_ids;
 
     constructor() ERC1155("") {}
+
+    function uploadImage(
+        uint256 price,
+        string calldata name,
+        string calldata description,
+        string calldata uri,
+        uint256 expiry
+    ) external {
+        StockImage memory new_image = StockImage({
+            id: latest_id,
+            name: name,
+            description: description,
+            uri: uri,
+            price: price,
+            creator: msg.sender,
+            expiry: expiry
+        });
+        stock_images.push(new_image);
+        creator_to_stock_image_ids[msg.sender].push(latest_id);
+        latest_id++;
+    }
 }
