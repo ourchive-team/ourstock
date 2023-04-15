7a5ff28fb9e2a4142f7241bb52e4fb7bc064b7ea
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-15 15:30:26 +0900

    Implement checking image name duplication

--- backend/src/Marketplace.sol
+++ backend/src/Marketplace.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.8.17;
 
 import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
+error DuplicatedImageName();
+
 struct StockImage {
     uint256 id;
     string name;
@@ -26,6 +28,7 @@ contract Marketplace is ERC1155 {
         string calldata uri,
         uint256 expiry
     ) external {
+        checkDuplicateName(msg.sender, name);
         StockImage memory new_image = StockImage({
             id: latest_id,
             name: name,
@@ -38,5 +41,22 @@ contract Marketplace is ERC1155 {
         stock_images.push(new_image);
         creator_to_stock_image_ids[msg.sender].push(latest_id);
         latest_id++;
+    }
+
+    function checkDuplicateName(
+        address creator,
+        string calldata name
+    ) private view {
+        uint256[] memory image_ids = creator_to_stock_image_ids[creator];
+
+        for (uint256 i = 0; i < image_ids.length; i++) {
+            string memory image_name = stock_images[image_ids[i]].name;
+            if (
+                keccak256(abi.encodePacked(image_name)) ==
+                keccak256(abi.encodePacked(name))
+            ) {
+                revert DuplicatedImageName();
+            }
+        }
     }
 }
